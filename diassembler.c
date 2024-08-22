6b83b88911094fa5f23aca6fe268d37ba29f175f
luizangomes <luizangomes@outlook.com>
2024-08-22 12:19:45 -0300

    diassembler start point

--- diassembler.c
+++ diassembler.c
@@ -4,6 +4,8 @@
 
 #include "diassembler.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct OpcodesAssembly {
     char opcodeSimbolico[30];
@@ -11,6 +13,17 @@ struct OpcodesAssembly {
     int tamanhoEmPalavras;
     char acao[100];
     int numeroDeOperandos;
+};
+
+struct AddressObjCode {
+    int address;
+    int objCode;
+};
+
+struct programAssemblyInventado{
+    char opcode[50];
+    char operand1[50];
+    char operand2[50];
 };
 
 struct OpcodesAssembly tabelaDeInstrucoes[15] = {
@@ -31,9 +44,89 @@ struct OpcodesAssembly tabelaDeInstrucoes[15] = {
     {"STOP", 14, 1, "Suspende a execução", 0}
 };
 
+void tradutor(char* filename){
+    FILE *file;
+    struct AddressObjCode objCode[100];
+    struct programAssemblyInventado program1[100];
+    char line[1000], *token;
+    int listOfOperands[100], opIndex=0;
+    int lineCount = 0;
+    int address = 0;
+    int opcode = 0, stopOpcode = 0;
+    int opcodeBool = 0, opcodeOperands = 0, readOperand = 0;
+    // Open the file for reading
+
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(1);
+    }
+    while (fgets(line, 1000, file) != NULL) {
+        printf("%s\n", line);
+        char *token = strtok(line, " ");
+        while (token != NULL) {
+            objCode[address].address = address;
+            objCode[address].objCode = atoi(token);
+            if (objCode[address].objCode < 15 && objCode[address].objCode > 0 && stopOpcode == 0){
+                opcodeBool = 1;
+                opcode = objCode[address].objCode;
+                strcpy(program1[lineCount].opcode, token);
+                if ((opcode > 0 && opcode < 9) || (opcode > 9 && opcode < 14)){
+                    opcodeOperands = 1;
+                    readOperand = 1;
+                } else if (opcode == 9){
+                    opcodeOperands = 2;
+                    readOperand = 1;
+                } else if (opcode == 14){
+                    opcodeOperands = 0;
+                    readOperand = 0;
+                    stopOpcode = 1;
+                    strcpy(program1[lineCount].opcode, token);
+                    strcpy(program1[lineCount].operand1,  "");
+                    strcpy(program1[lineCount].operand2, "");
+                    printf("%s|%s|%s\n", program1[lineCount].opcode, program1[lineCount].operand1, program1[lineCount].operand2);
+                    lineCount += 1;
+                }
+            }
+            if (readOperand > 0 && opcodeBool == 0){
+                opcode = 0;
+                listOfOperands[opIndex] = atoi(token);
+                opIndex++;
+                if (opcodeOperands == 1){
+                    if (strcmp(program1[lineCount].opcode, "9") == 0){
+                        strcpy(program1[lineCount].operand2, token);
+
+                    } else {
+                        strcpy(program1[lineCount].operand1,  token);
+                        strcpy(program1[lineCount].operand2, "");
+                    }
+                    printf("%s|%s|%s\n", program1[lineCount].opcode, program1[lineCount].operand1, program1[lineCount].operand2);
+                    lineCount += 1;
+                    opcodeOperands -= 1;
+                }
+                if (opcodeOperands == 2){
+                    strcpy(program1[lineCount].operand1, token);
+                    opcodeOperands -= 1;
+                }
+            }
+            if (stopOpcode == 1 && opcodeBool == 0){
+                strcpy(program1[lineCount].opcode, "");
+                strcpy(program1[lineCount].operand1,  token);
+                strcpy(program1[lineCount].operand2, "");
+                printf("%s|%s|%s\n", program1[lineCount].opcode, program1[lineCount].operand1, program1[lineCount].operand2);
+                lineCount += 1;
+            }
+            //printf("        %d : %d\n", objCode[address].address, objCode[address].objCode);
+            address += 1;
+            opcodeBool = 0;
+            token = strtok(NULL, " ");
+        }
+    }
+}
+
 
 int main(int argc, char** argv) {
-    // char* filename = argv[1];
-
+    char* filename = argv[1];
+    tradutor(filename);
     return 0;
 }
